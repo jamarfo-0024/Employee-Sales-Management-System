0aaef64e15ca85e28c2a66e5d8d3826927d6ae7e
jamarfo-oo24 <judeasantemarfo@gmail.com>
2026-02-15 01:10:25 +0000

    Add role-based queryset filtering for employee and customer APIs

--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,16 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Customer
 from .serializers import CustomerSerializer
 
 class CustomerViewSet(viewsets.ModelViewSet):
-    queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role == 'ADMIN':
+            return Customer.objects.all()
+
+        return Customer.objects.filter(assigned_employee__user=user)
--- employees/views.py
+++ employees/views.py
@@ -1,7 +1,16 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Employee
 from .serializers import EmployeeSerializer
 
 class EmployeeViewSet(viewsets.ModelViewSet):
-    queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role == 'ADMIN':
+            return Employee.objects.all()
+
+        return Employee.objects.filter(user=user)
