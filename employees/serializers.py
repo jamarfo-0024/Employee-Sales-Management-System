77b5abd09ae4df1653bf8a5d7b9ca39e603b3459
jamarfo-oo24 <judeasantemarfo@gmail.com>
2026-02-22 19:08:46 +0000

    Add nested serializers for employee customers and sales with computed commission totals

--- employees/serializers.py
+++ employees/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
 from .models import Employee
+from customers.serializers import CustomerSerializer
+from sales.serializers import SaleSerializer
+
 
 class EmployeeSerializer(serializers.ModelSerializer):
+
+    customers = CustomerSerializer(
+        many=True,
+        read_only=True,
+        source='customers'
+    )
+
+    sales = SaleSerializer(
+        many=True,
+        read_only=True
+    )
+
+    total_commission_earned = serializers.SerializerMethodField()
+
     class Meta:
         model = Employee
         fields = '__all__'
+
+    def get_total_commission_earned(self, obj):
+        return sum(sale.commission for sale in obj.sales.all())
